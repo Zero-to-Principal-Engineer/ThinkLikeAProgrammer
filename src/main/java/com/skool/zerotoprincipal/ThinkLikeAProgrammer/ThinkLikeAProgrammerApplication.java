4e0fab40143a3fe8ca40de820453685e2154784d
wizardbeard <ramonjlongiii@gmail.com>
2024-08-19 20:36:52 -0700

    feat: initial commit

---
+++ src/main/java/com/skool/zerotoprincipal/ThinkLikeAProgrammer/ThinkLikeAProgrammerApplication.java
@@ -0,0 +1,14 @@
+package com.skool.zerotoprincipal.ThinkLikeAProgrammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkLikeAProgrammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkLikeAProgrammerApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/skool/zerotoprincipal/ThinkLikeAProgrammer/ThinkLikeAProgrammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skool.zerotoprincipal.ThinkLikeAProgrammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkLikeAProgrammerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
